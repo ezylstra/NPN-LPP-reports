9ae59d0a1871ad76406292e1e69f9bd8c831ceb5
Erin Zylstra <erinzylstra@gmail.com>
2024-11-27 09:36:46 -0700

    Add annotation and change pheno group names

--- phenophases-intensities.R
+++ phenophases-intensities.R
@@ -21,10 +21,10 @@ ph <- npn_phenophases() %>%
          category = phenophase_category) %>% 
   data.frame() 
 # Returns dataframe with phenophase id, name, category, and class_id
-# Note: phenophase_name does not always match up with phenophase_description
-# in the status-intensity datasets because the descriptions often tack on a 
-# parenthetical reference to a taxonomic group (eg, forbs, birds).
-# Note: many trailing white spaces in phenophase_name entries
+  # Note: phenophase_name does not always match up with phenophase_description
+  # in the status-intensity datasets because the descriptions often tack on a 
+  # parenthetical reference to a taxonomic group (eg, forbs, birds).
+  # Note: many trailing white spaces in phenophase_name entries
 
 # Extract information about pheno classes (higher-level order of phenophases)
 phc <- npn_pheno_classes() %>% 
@@ -32,49 +32,61 @@ phc <- npn_pheno_classes() %>%
          class_name = name,
          class_description = description) %>%
   data.frame()
-# Returns dataframe with pheno class id, name, description, sequence
+# Returns dataframe with pheno class id, name, description, sequence.
+  # class_id = 1:13 are the phenophase classes associated with plants. Sequence
+  # is just class_id * 10, and both generally reflect the order in which things
+  # occur within a category (eg, Leaves). Based on exploration of 
+  # status-intensity data for a particular plant species and class_id, there is
+  # only one phenophase per phenophase class for a given species, which is
+  # helpful. 
+  # class_id's > 60 are associated with animals. Many more phenophase classes
+  # for animals than plants and the relationship between class_id and sequence
+  # is not obvious.
 
 # Merge the two
-ph <- left_join(ph, phc, by = "class_id")
+ph <- left_join(ph, phc, by = "class_id") %>%
+  arrange(category, class_id, phenophase_id)
 
-# count(ph, category)
-# filter(ph, category == "Activity")     # Animal categories 
-# filter(ph, category == "Development")  # Animal, by stage (includes Dead categories!)
-# filter(ph, category == "Method")       # Animals captured or baited
-# filter(ph, category == "Reproduction") # Any animal reproduction-related stage (including eggs)
-# filter(ph, category == "Flowers")      # Any flower-related stage (buds to senescense)
-# filter(ph, category == "Fruits")       # Any fruit-related stage (first to drop)
-# filter(ph, category == "Leaves")       # Any leaf-related stage (breaking buds to drop)
-# filter(ph, category == "Needles")      # Any needle-reated stage (budburst to drop)
-# filter(ph, category == "Pollen cones") # Any cone-related stage (first to pollen release)
-# filter(ph, category == "Seed cones")   # Any seed cone-related stage (first to drop)
+# Look at phenophases, classes associated with each category
+  count(ph, category)
+  filter(ph, category == "Development")  # Animal, by stage (includes Dead categories!)
+  filter(ph, category == "Method")       # Animals captured or baited
+  filter(ph, category == "Reproduction") # Any animal reproduction-related stage (including eggs)
+  filter(ph, category == "Activity")     # All other animal phenophases
+  filter(ph, category == "Flowers")      # Any flower-related stage (buds to senescense)
+  filter(ph, category == "Fruits")       # Any fruit-related stage (first to drop)
+  filter(ph, category == "Leaves")       # Any leaf-related stage (breaking buds to drop)
+  filter(ph, category == "Needles")      # Any needle-reated stage (budburst to drop)
+  filter(ph, category == "Pollen cones") # Any cone-related stage (first to pollen release)
+  filter(ph, category == "Seed cones")   # Any seed cone-related stage (first to drop)
 
-# For summarizing animal data, 
-# Dead observation = Any dead observations
-# Live observation = Any non-dead phenophase in Activity, Development, Reproduction categories
-#TODO: Decide whether to leave observations in Methods category separate?
-#TODO: Decide whether it's okay to lump eggs, pupae, caterpillars with adults
+# Proposed pheno "groups" for animals
+  # Dead observation = Any dead observations
+  # Live observation = Any non-dead phenophase in Activity, Development, Reproduction categories
+  #TODO: Decide whether to leave observations in Methods category separate?
+  #TODO: Decide whether it's okay to lump eggs, pupae, caterpillars with adults
 
-# For summarizing plant data, the classes seem too broad, but phenophase_name
-# seems to detailed. Creating an intermediate classification for LPP reports 
-# (mostly based on categories that were used previously by Janet Prevey):
+# Prosed pheno "groups" for plants
+  # For summarizing plant data, the classes seem too broad, but phenophase_name
+  # seems too detailed. Creating an intermediate classification for LPP reports 
+  # (largely based on categories that were used previously by Janet Prevey):
+  
+  ph %>%
+    filter(category %in% c("Leaves", "Needles")) %>%
+    arrange(class_id)
+  ph %>%
+    filter(category %in% c("Flowers", "Pollen cones", "Fruits", "Seed cones")) %>%
+    arrange(class_id)
+  
+  # Leaves: classes 1:3 = Initial shoot or leaf growth, Young leaves or needles, Leaves or needles (Janet called this Leaf out)
+  # Leaf senescence: class 4:5 = Colored leaves or needles, Falling leaves or needles
+  # Flowers: class 6 = Flowers or pollen cones (Janet called this Flower buds)
+  # Open flowers: classes 7:8 = Open flowers or pollen cones, Pollen release (Janet called this Flowering)
+  # Flower senescence: class 9 = End of flowering
+  # Fruits: classes 10:11 = Fruits or seed cones, Unripe fruits or seed cones (Janet called this Unripe fruit)
+  # Ripe fruits: classes 12:13 = Ripe fruits or seed cones, Recent fruit, cone or seed drop
 
-ph %>%
-  filter(category %in% c("Flowers", "Pollen cones", "Fruits", "Seed cones")) %>%
-  arrange(class_id)
-# Flower buds: class = Flowers or pollen cones
-# Flowering: class = Open flowers or pollen cones, Pollen release
-# Flower senescence: class = End of flowering
-# Unripe fruits: class = Fruits or seed cones, Unripe fruits or seed cones
-# Ripe fruits: class = Ripe fruits or seed cones, Recent fruit, cone or seed drop
-
-ph %>%
-  filter(category %in% c("Leaves", "Needles")) %>%
-  arrange(class_id)
-# Leaf out: class = Initial shoot or leaf growth, Young leaves or needles, Leaves or needles
-# Leaf senescence: class = Colored leaves or needles, Falling leaves or needles
-
-# Create new "pheno_group"
+# Create new "pheno_group" 
 ph <- ph %>%
   mutate(pheno_group = case_when(
     grepl("dead|Dead", phenophase_name) ~ "Dead observation",
@@ -82,17 +94,17 @@ ph <- ph %>%
                     "Development", 
                     "Reproduction") ~ "Live observation",
     category == "Method" ~ "Trapped/baited",
-    class_name == "Flowers or pollen cones" ~ "Flower buds",
+    class_name == "Flowers or pollen cones" ~ "Flowers",
     class_name %in% c("Open flowers or pollen cones", 
-                      "Pollen release") ~ "Flowering",
+                      "Pollen release") ~ "Open flowers",
     class_name == "End of flowering" ~ "Flower senescence",
     class_name %in% c("Fruits or seed cones", 
-                      "Unripe fruits or seed cones") ~ "Unripe fruits",
+                      "Unripe fruits or seed cones") ~ "Fruits",
     class_name %in% c("Ripe fruits or seed cones", 
                       "Recent fruit, cone or seed drop") ~ "Ripe fruits",
     class_name %in% c("Initial shoot or leaf growth",
                       "Young leaves or needles",
-                      "Leaves or needles") ~ "Leaf out",
+                      "Leaves or needles") ~ "Leaves",
     class_name %in% c("Colored leaves or needles",
                       "Falling leaves or needles") ~ "Leaf senescence",
     .default = NA
